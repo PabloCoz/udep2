4e430d8e2a0f27a15571a7bb8b2b5b7f5620efe8
PabloCoz <pablocr_234@hotmail.com>
2022-04-20 15:10:05 -0500

    nineth commit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,9 +16,9 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $role = Role::create(['name' => 'Admin']);
-        $role->permissions()->attach([1, 2, 3, 4, 5, 6, 7,8,9,10,11,12]);
+        $role->permissions()->attach([1, 2, 3, 4, 5, 6, 7,8,9,10,11]);
 
         $role = Role::create(['name' => 'Psychologist']);
-        $role->syncPermissions(['Crear Examen', 'Listar Examen', 'Editar Examen', 'Eliminar Examen', 'Ver Respuestas']);
+        $role->syncPermissions(['Crear Examen', 'Listar Examen', 'Editar Examen', 'Ver Respuestas']);
     }
 }
